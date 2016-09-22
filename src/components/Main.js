bf43aa084fe5093b31b6f56ea68343556836799b
chenzhicai <18616952720@163.com>
2016-09-22 17:46:04 +0800

    更新

--- src/components/Main.js
+++ src/components/Main.js
@@ -17,10 +17,11 @@ class AppComponent extends React.Component {
         var controllerUnits = [],
             ifDatas = [];
 
-        imageDatas.forEach(function(value,index) {
+        imageDatas.forEach(function(value, index) {
                 ifDatas.push( < ImgFigure data = {
                         value
-                    } ref = {'imgFigure' + index}
+                    }
+                    ref = { 'imgFigure' + index }
                     />);
                 });
             return ( < section className = "state" >
@@ -49,6 +50,14 @@ AppComponent.Constant = {
     vPosRange: {
         x: [0, 0],
         topY: [0, 0]
+    },
+
+    /*
+    *重新布局所有图片
+    *@param centerIndex 指定居中排布哪个图片
+     */
+    rearrange: function  (centerIndex) {
+
     }
 }
 
@@ -61,6 +70,33 @@ AppComponent.componentDidMount = function() {
         halfStageW = Math.ceil(stageW / 2),
         halfStageH = Math.ceil(stageH / 2);
 
-    //
+    // 拿到一个imageFigure的大小
+    var imgFigureDOM = React.findDOMNode(this.refs.imgsFigure0),
+        imgW = imgFigureDOM.scrollWidth,
+        imgH = imgFigureDOM.scrollHeight,
+        halfImgW = Math.ceil(imgW / 2),
+        halfImgH = Math.ceil(imgH / 2);
+
+    // 计算中心图片的位置点
+    this.Constant.centerPos = {
+        left: halfStageW - halfImgW,
+        top: halfStageH - halfImgH
+    }
+
+    // 计算左侧,右侧区域图片排布位置的取值范围
+    this.Constant.hPosRange.leftSecx[0] = -halfImgW;
+    this.Constant.hPosRange.leftSecx[1] =halfStageW - halfImgW*3;
+    this.Constant.hPosRange.rightSecx[0] =halfStageW = halfImgW;
+    this.Constant.hPosRange.rightSecx[1] =stageW - halfImgW;
+    this.Constant.hPosRange.y[0] = -halfImgH;
+    this.Constant.hPosRange.y[1] = stageH - halfImgH;
+
+    // 计算上侧区域图片排布位置的取值范围
+    this.Constant.vPosRange.toyY[0] = -halfImgH;
+    this.constant.vPosRange.topY[1] = halfStageH - halfImgH * 3;
+    this.Constant.vPosRange.x[0] = halfImgW - imgFigureDOM;
+    this.constant.vPosRange.x[1] = halfImgW;
+
+    this.rearrange(0);
 }
 export default AppComponent;
